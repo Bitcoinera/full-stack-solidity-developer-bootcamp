494385e6ccf9349db76fa53c57f5488845d4ad49
Bitcoinera <anagjordano@gmail.com>
2022-06-10 19:51:19 +0200

    tests running + gas reporter

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.8",
+  gasReporter: {
+    enabled: true,
+    // currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
 };
