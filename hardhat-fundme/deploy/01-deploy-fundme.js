7fa576236f71944b56c21fd5234ec0a056ea1818
Bitcoinera <anagjordano@gmail.com>
2022-06-11 17:56:46 +0200

    deploy in rinkeby + add verify

--- hardhat-fundme/deploy/01-deploy-fundme.js
+++ hardhat-fundme/deploy/01-deploy-fundme.js
@@ -1,5 +1,7 @@
+require("dotenv").config()
 const { network } = require("hardhat")
 const { networkConfig, developmentChains } = require("../hardhat-deploy-helper")
+const { verify } = require("../utils/verify")
 
 module.exports.default = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -16,14 +18,24 @@ module.exports.default = async ({ getNamedAccounts, deployments }) => {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
 
-    await deploy("FundMe", {
+    const fundMe = await deploy("FundMe", {
         constract: "FundMe",
         from: deployer,
         log: true,
         args: [ethUsdPriceFeedAddress],
+        // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
-    log("FundMe deployed!")
+    log(`FundMe deployed at ${fundMe.address}!`)
     log("----------------------------------------------------")
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_APIKEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+        log("Contract was successfully verified!")
+    }
 }
 
 module.exports.tags = ["all", "fundme"]
--- hardhat-fundme/hardhat.config.js
+++ hardhat-fundme/hardhat.config.js
@@ -6,41 +6,43 @@ require("hardhat-gas-reporter")
 require("hardhat-deploy")
 require("solidity-coverage")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || ""
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
-    for (const account of accounts) {
-        console.log(account.address)
-    }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
     solidity: {
         compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
     },
+    defaultNetwork: "hardhat",
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        kovan: {
+            url: KOVAN_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 42,
+            blockConfirmations: 6,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
         },
     },
     gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
+        enabled: true,
         currency: "USD",
+        outputFile: "gas-report.txt",
+        // coinmarketcap: COINMARKETCAP_API_KEY,
     },
     etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY,
     },
     namedAccounts: {
         deployer: {
